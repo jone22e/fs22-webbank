a2664c147876cf8dfb5285b511df7ea0dec05866
Jone <ti@brasiltec.ind.br>
2021-11-30 16:39:35 -0300

    ...

--- index.php
+++ index.php
@@ -9,7 +9,11 @@ foreach ($clientes as $cliente) {
     echo 'Tempo Trabalhando: '.round((intval($cliente->statistics->workedTime[0])/60), 2).' horas<br>';
     echo 'Hectares trabalhados : '.$cliente->statistics->workedHectares[0].'<br>';
     echo 'Uso de Combustível: '.$cliente->statistics->fuelUsage[0].'<br>';
+    echo 'Sementes Usadas: '.$cliente->statistics->seedUsage[0].'<br>';
     echo 'Distância Percorrida de Trator: '.$cliente->statistics->tractorDistance[0].'<br>';
+    echo 'Arvores Cortadas: '.$cliente->statistics->cutTreeCount[0].'<br>';
+    echo 'Arvores Plantadas: '.$cliente->statistics->plantedTreeCount[0].'<br>';
+    echo 'Corsertos de Veículos: '.$cliente->statistics->repairVehicleCount[0].'<br>';
 
     echo '<br>';
 }
